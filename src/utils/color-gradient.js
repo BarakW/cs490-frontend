3fe9d1ce121fdd23d945f0f013d8034699e227ab
sameesiddiqui <sameezsiddiqui@gmail.com>
2019-04-04 19:22:06 -0400

    default hsl fix

--- src/utils/color-gradient.js
+++ src/utils/color-gradient.js
@@ -5,7 +5,7 @@ export const convertNumToColor = (num, minNum = 0) => {
     const color2 = {hue: 120, saturation: 100, lightness: 30};
     const percent = num / 100;
 
-    if (num < minNum) return "hsl("+color1.hue+","+color1.saturation+","+color1.lightness+")";
+    if (num < minNum) return "hsl("+color1.hue+","+color1.saturation+"%,"+color1.lightness+"%)";
     const h = color1.hue + percent * (color2.hue - color1.hue);
     const s = color1.saturation + percent * (color2.saturation - color1.saturation);
     const l = color1.lightness + percent * (color2.lightness - color1.lightness);
