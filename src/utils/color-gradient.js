664b98e0784299506db8d04fa6c21ed6278ce7ba
Barak Widawsky <6716701+BarakW@users.noreply.github.com>
2019-04-04 19:16:07 -0400

    Better colors

--- src/utils/color-gradient.js
+++ src/utils/color-gradient.js
@@ -1,15 +1,15 @@
 // converts a movie rating to a color
 // minNum is the min score for it to not be the most red
 export const convertNumToColor = (num, minNum = 0) => {
-    const color1 = {red: 170, green: 0, blue: 0};
-    const color2 = {red: 0, green: 150, blue: 0};
+    const color1 = {hue: 0, saturation: 100, lightness: 33};
+    const color2 = {hue: 120, saturation: 100, lightness: 30};
     const percent = num / 100;
 
-    if (num < minNum) return "rgb("+color1.red+","+color1.green+","+color1.blue+")";
-    const r = color1.red + percent * (color2.red - color1.red);
-    const g = color1.green + percent * (color2.green - color1.green);
-    const b = color1.blue + percent * (color2.blue - color1.blue);
+    if (num < minNum) return "hsl("+color1.hue+","+color1.saturation+","+color1.lightness+")";
+    const h = color1.hue + percent * (color2.hue - color1.hue);
+    const s = color1.saturation + percent * (color2.saturation - color1.saturation);
+    const l = color1.lightness + percent * (color2.lightness - color1.lightness);
 
-    // return a rgb string that can be used in styling of a component
-    return "rgb("+r+","+g+","+b+")";
+    // return a hsl string that can be used in styling of a component
+    return "hsl("+h+","+s+"%,"+l+"%)";
 }
